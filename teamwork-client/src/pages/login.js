0a08b58f4136f21cf5df078450b0b9e000ec89cb
Digdarshan Kunwar <digdarshan.kunwar@gmail.com>
2020-02-20 23:31:26 +0100

    :boom: Client: Setting up frontend environment.

---
+++ teamwork-client/src/pages/login.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class login extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is a Login page</h1>
+      </div>
+    );
+  }
+}
+
+export default login;
